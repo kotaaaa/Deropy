1094ef3951032f0266fc246c58e86bc6a9da8f4b
Shintaro Okada <shintaro@Mac2.local>
2018-08-02 17:31:50 +0900

    a

--- google.py
+++ google.py
@@ -15,14 +15,11 @@ def get_images(keyword, result_dir, maximum=100):
 
     # キーワードを"%??..."の形式に変換
     urlKeyword = parse.quote(keyword)
-    
+
     url = 'https://www.google.com/search?hl=jp&q=' + \
         urlKeyword + '&btnG=Google+Search&tbs=0&safe=off&tbm=isch'
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-    # request = req.Request(url=url, headers=headers)
-    # page = req.urlopen(request)
-    # html = page.read().decode('utf-8')
     html = requests.get(url=url, headers=headers).text
     soup = bs4.BeautifulSoup(html, "html.parser")
     elems = soup.select('.rg_meta.notranslate')
